f2ae8acddb00ec094689d39768083b99ff48dbef
= <=>
2020-04-09 14:11:41 +0200

    Added Handwritingconfiguration

---
+++ HandwritingService.DAL/Configurations/HandwritingConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using HandwritingService.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.IO;
+
+namespace HandwritingService.DAL.Configurations
+{
+    class HandwritingConfiguration : IEntityTypeConfiguration<Handwriting>
+    {
+        public void Configure(EntityTypeBuilder<Handwriting> builder)
+        {
+            builder
+                .HasOne(h => h.Note)
+                .WithOne(n => n.Handwriting)
+                .HasForeignKey<Note>(n => n.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasData(new Handwriting()
+            {
+                Id = 1,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+                Image = GetImage(),
+                NoteId = 1,
+            }); ;
+
+        }
+
+        public byte[] GetImage()
+        {
+            var image = Image.FromFile("./Assets/text.jpeg");
+
+            using var stream = new MemoryStream();
+
+            image.Save(stream, image.RawFormat);
+            return stream.ToArray();
+
+        }
+    }
+}
--- HandwritingService.Domain/Note.cs
+++ HandwritingService.Domain/Note.cs
@@ -7,6 +7,8 @@ namespace HandwritingService.Domain
     public class Note
     {
         public int Id { get; set; }
+
+        public Handwriting Handwriting { get; set; }
         public int HandwritingId { get; set; }
     }
 }
