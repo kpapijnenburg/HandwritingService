5f070fb002802ac275046ebd168d98688dfa87d4
= <=>
2020-04-27 16:10:35 +0200

    Added Logic project

---
+++ HandwritingService.Logic/Abstract/ITextExtractor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandwritingService.Logic.Abstract
+{
+    public interface ITextExtractor
+    {
+        Task<string> FromImage(byte[] imageData);
+    }
+}
---
+++ HandwritingService.Logic/DummyTextExtractor.cs
@@ -0,0 +1,16 @@
+﻿using HandwritingService.Logic.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandwritingService.Logic
+{
+    public class DummyTextExtractor : ITextExtractor
+    {
+        public Task<string> FromImage(byte[] imageData)
+        {
+            return Task.FromResult("Dummy \n Text \n Extractor");
+        }
+    }
+}
