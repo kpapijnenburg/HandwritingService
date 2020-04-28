63529852cc7f6ca786d6ef197d93ef0844f9e20b
= <=>
2020-04-28 15:58:36 +0200

    Setup tests

---
+++ HandwritingService.Tests/NoteCreatedMessageConsumerTests.cs
@@ -0,0 +1,43 @@
+﻿using BIED.Messaging;
+using BIED.Messaging.Abstractions;
+using HandwritingService.Domain;
+using HandwritingService.Logic.Abstract;
+using HandwritingService.Web.Messaging.Consumers;
+using HandwritingService.Web.Messaging.Messages;
+using KPA.Database.Abstractions;
+using Moq;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HandwritingService.Tests
+{
+    public class NoteCreatedMessageConsumerTests
+    {
+        private readonly NoteCreatedMessageConsumer consumer;
+
+        private readonly Mock<IConnection> connectionMock;
+        private readonly Mock<IMessageProducer> messageProducerMock;
+        private readonly Mock<ITextExtractor> textExtractorMock;
+        private readonly Mock<IRepository<Handwriting>> repositoryMock;
+
+        public NoteCreatedMessageConsumerTests()
+        {
+            connectionMock = new Mock<IConnection>();
+            messageProducerMock = new Mock<IMessageProducer>();
+            textExtractorMock = new Mock<ITextExtractor>();
+            repositoryMock = new Mock<IRepository<Handwriting>>();
+
+            this.consumer = new NoteCreatedMessageConsumer(connectionMock.Object, messageProducerMock.Object, textExtractorMock.Object, repositoryMock.Object);
+
+        }
+
+        [Fact]
+        public async Task Test_Test()
+        {
+        }
+    }
+}
